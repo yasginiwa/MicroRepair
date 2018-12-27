7971c79feaf8427c2e00417a1c9d4d0d8233fa74
yglee <23801504@qq.com>
2018-12-27 14:44:35 +0800

    增加记录本地存储

--- pages/record/record.js
+++ pages/record/record.js
@@ -3,8 +3,9 @@ var api = require('../../utils/apiUtil.js');
 
 Page({
 
-  data:{
-    deviceRecords: []
+  data: {
+    deviceRecords: [],
+    page: 0
   },
 
   /**
@@ -14,9 +15,9 @@ Page({
     var deviceRecord = JSON.stringify(this.data.deviceRecords[e.currentTarget.dataset.index]);
     wx.navigateTo({
       url: '../deviceRecordDetail/deviceRecordDetail?deviceRecord=' + deviceRecord,
-      success: function (res) { },
-      fail: function (res) { },
-      complete: function (res) { },
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
     })
   },
 
@@ -42,6 +43,21 @@ Page({
       console.log(e);
     }
 
+    //  从本地获取deviceRecordArray
+    try {
+      var deviceRecordArray = wx.getStorageSync('deviceRecordArray');
+    } catch (e) {
+      console.log(e);
+    }
+
+    if(!deviceRecordArray) {
+      try {
+      wx.setStorageSync(deviceRecordArray, []);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
     if (nickName == null || token == null) {
       wx.showToast({
         title: '请登录系统...',
@@ -52,26 +68,33 @@ Page({
         deviceRecords: []
       })
       return;
-    } 
+    }
 
     var that = this;
-    var deviceRecordArray = that.data.deviceRecords;
+
     wx.request({
       url: api.userrecordUrl,
       method: 'POST',
       data: {
         engineer: nickName,
+        page: 0,
         token: token
       },
-      success: function (res) {
-
-        deviceRecordArray = deviceRecordArray.concat(res.data.result);
+      success: function(res) {
         that.setData({
-          deviceRecords: deviceRecordArray
+          deviceRecords: res.data.result.concat(deviceRecordArray)
         })
+
+        // 隐藏toast
         wx.hideLoading();
+
+        //  存到deviceRecordArray本地
+        wx.setStorage({
+          key: 'deviceRecordArray',
+          data: that.data.deviceRecords,
+        })
       },
-      fail: function (res) {
+      fail: function(res) {
         wx.hideLoading();
         wx.showToast({
           title: '网络超时,请检查网络设置!',
@@ -82,24 +105,10 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.requestData();
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  loadMore() {
+    wx.showLoading({
+      title: '加载中...',
+    })
 
     //  从本地获取userInfo信息
     try {
@@ -115,44 +124,119 @@ Page({
       console.log(e);
     }
 
-   if (this.data.deviceRecords.length == 0 || nickName == null || token == null) {
-     this.requestData();
-   } else return;
+    //  从本地获取deviceRecordArray
+    try {
+      var deviceRecordArray = wx.getStorageSync('deviceRecordArray');
+    } catch (e) {
+      console.log(e);
+    }
+
+    if (nickName == null || token == null) {
+      wx.showToast({
+        title: '请登录系统...',
+        icon: 'none',
+        mask: true
+      })
+      this.setData({
+        deviceRecords: []
+      })
+      return;
+    }
+
+    // 上拉 页面自增
+    this.setData({
+      page: this.data.page + 1
+    })
+
+    var that = this;
+    wx.request({
+      url: api.userrecordUrl,
+      method: 'POST',
+      data: {
+        engineer: nickName,
+        page: that.data.page,
+        token: token
+      },
+      success: function(res) {
+        deviceRecordArray = deviceRecordArray.concat(res.data.result);
+        that.setData({
+          deviceRecords: deviceRecordArray
+        })
+        wx.hideLoading();
+
+        //  存储deviceRecordArray到本地
+        wx.setStorage({
+          key: 'deviceRecordArray',
+          data: that.data.deviceRecords,
+        })
+      },
+      fail: function(res) {
+        wx.hideLoading();
+        wx.showToast({
+          title: '网络超时,请检查网络设置!',
+          icon: 'none',
+          mask: true
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.requestData();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    wx.startPullDownRefresh();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    // this.requestData();
-    // wx.stopPullDownRefresh();
+  onPullDownRefresh: function() {
+    console.log('下拉刷新');
+    this.requestData();
+    wx.stopPullDownRefresh();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
+  onReachBottom: function() {
+    console.log('上拉加载');
+    this.loadMore();
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
